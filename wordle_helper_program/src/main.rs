47b51d85aa2c616d992b1dc28573d7c42d546166
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-06-17 19:50:07 +0200

    Rust project created

---
+++ wordle_helper_program/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    //Show a green "Hello, world!" in the terminal
+    println!("\x1b[32mWORDLE HELPER PROGRAM\x1b[0m");
+}
---
+++ wordle_helper_program/target/debug/wordle_helper_program.d
@@ -0,0 +1,1 @@
+/Users/parzival1918/Documents/GitHub/WordleHelper/wordle_helper_program/target/debug/wordle_helper_program: /Users/parzival1918/Documents/GitHub/WordleHelper/wordle_helper_program/src/main.rs
