9ab27d9f5798d433fe2a98f1eebb97489a744bfe
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2023-07-01 09:44:19 +0200

    feat: Add colour support

--- wordle-helper.py
+++ wordle-helper.py
@@ -26,10 +26,23 @@ class Letter:
     def __repr__(self): #Printable representation of the object
         return f"{self.letter} - {self.position} - {self.correct}"
     
+#Text colour class
+class TextColour:
+   PURPLE = '\033[1;35;48m'
+   CYAN = '\033[1;36;48m'
+   BOLD = '\033[1;37;48m'
+   BLUE = '\033[1;34;48m'
+   GREEN = '\033[1;32;48m'
+   YELLOW = '\033[1;33;48m'
+   RED = '\033[1;31;48m'
+   BLACK = '\033[1;30;48m'
+   UNDERLINE = '\033[4;37;48m'
+   END = '\033[1;37;0m'
+    
 #Function to print possible words
 def printPossibleWords(possibleWords: list):
     if len(possibleWords) == 0:
-        print("No words found with this combination")
+        print(TextColour.RED + "No words found with this combination" + TextColour.END)
         return
 
     for word in possibleWords:
